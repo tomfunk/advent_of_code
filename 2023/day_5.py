c7b93f89209ec9cdfbde018521e2214d6b7a5385
Thomas <thomas@keepertax.com>
2024-01-06 10:34:06 -0800

    stuck on part 2

--- 2023/day_5.py
+++ 2023/day_5.py
@@ -54,18 +54,40 @@ def parse_lines(lines):
         source_start, dest_start, n = int(source_start), int(dest_start), int(n)
         
         maps[current_map].append({
-            "range_start": source_start,
-            "range_end": source_start + n,
+            "source_start": source_start,
+            "source_end": source_start + n,
             "adjustment": dest_start - source_start,
+            "dest_start": dest_start,
+            "dest_end": dest_start + n,
         })
 
     return seeds, maps
 
+
 def get_map_output(value, value_maps):
     for value_map in value_maps:
-        if value >= value_map["range_start"] and value < value_map["range_end"]:
+        if value >= value_map["source_start"] and value < value_map["source_end"]:
             return value + value_map["adjustment"]
     return value
+
+
+def get_single_lookup(seed, maps):
+    single_lookup = dict()
+    value = get_map_output(seed, maps["seed-to-soil"])
+    single_lookup["soil"] = value
+    value = get_map_output(value, maps["soil-to-fertilizer"])
+    single_lookup["fertilizer"] = value
+    value = get_map_output(value, maps["fertilizer-to-water"])
+    single_lookup["water"] = value
+    value = get_map_output(value, maps["water-to-light"])
+    single_lookup["light"] = value
+    value = get_map_output(value, maps["light-to-temperature"])
+    single_lookup["temperature"] = value
+    value = get_map_output(value, maps["temperature-to-humidity"])
+    single_lookup["humidity"] = value
+    value = get_map_output(value, maps["humidity-to-location"])
+    single_lookup["location"] = value
+    return single_lookup
 
 
 def get_lookup(lines):
@@ -73,30 +95,66 @@ def get_lookup(lines):
     seed_lookup = dict()
     for seed in seeds:
         seed = int(seed)
-        seed_lookup[seed] = dict()
-        value = get_map_output(seed, maps["seed-to-soil"])
-        seed_lookup[seed]["soil"] = value
-        value = get_map_output(value, maps["soil-to-fertilizer"])
-        seed_lookup[seed]["fertilizer"] = value
-        value = get_map_output(value, maps["fertilizer-to-water"])
-        seed_lookup[seed]["water"] = value
-        value = get_map_output(value, maps["water-to-light"])
-        seed_lookup[seed]["light"] = value
-        value = get_map_output(value, maps["light-to-temperature"])
-        seed_lookup[seed]["temperature"] = value
-        value = get_map_output(value, maps["temperature-to-humidity"])
-        seed_lookup[seed]["humidity"] = value
-        value = get_map_output(value, maps["humidity-to-location"])
-        seed_lookup[seed]["location"] = value
+        seed_lookup[seed] = get_single_lookup(seed, maps)
     return seed_lookup
+
 
 def get_min_location(lines):
     lookup = get_lookup(lines)
     return min((d["location"] for d in lookup.values()))
-    # return sorted(lookup.items(), key=lambda x: x[1]['location'])[0][0]
+
+
+def split_range(source_range, mapper):
+    lower, upper = source_range
+    split_points = [get_map_output(lower, mapper), get_map_output(upper, mapper)]
+    # split_points = [lower, upper]
+    for entry in mapper:
+        if lower < entry["source_start"] and upper >= entry["source_start"]:
+            # print(lower, "--->", entry["source_start"], "--->", upper)
+            split_value = get_map_output(entry["source_start"], mapper)
+            # split_value = entry["source_start"]
+            split_points += [split_value, split_value + 1]
+        if lower <= entry["source_end"] and upper > entry["source_end"]:
+            # print(lower, "--->", entry["source_end"], "--->", upper)
+            # split_value = entry["source_end"]
+            split_value = get_map_output(entry["source_end"], mapper)
+            split_points += [split_value, split_value + 1]
+            
+    split_points = sorted(list(set(split_points)))
+    return [(split_points[i], split_points[i + 1]) for i in range(0,
+    len(split_points), 2)]
+
+
+def get_range_lookup(lines):
+    seeds, maps = parse_lines(lines)
+
+    ranges = [(int(seeds[i]), int(seeds[i]) + int(seeds[i + 1])) for i in range(0, len(seeds), 2)]
+    map_names = [
+        "seed-to-soil",
+        "soil-to-fertilizer",
+        "fertilizer-to-water",
+        "water-to-light",
+        "light-to-temperature",
+        "temperature-to-humidity",
+        "humidity-to-location",
+    ]    
+    print("seeds")
+    print(ranges)
+    for map_name in map_names:
+        new_ranges = []
+        # split ranges
+        for r in ranges:
+            new_ranges += split_range(r, maps[map_name])
+            # ranges = [(get_map_output(p[0], mapper), get_map_output(p[1], mapper)) for p in split_points])
+            # print('before', ranges)
+            # print('after', [(get_map_output(p[0], mapper), get_map_output(p[1], mapper)) for p in split_points])
+        ranges = sorted(new_ranges)
+        print(map_name)
+        print(ranges)
 
 def main():
     print("sample 1:", get_min_location(sample))
+    print("sample 2:", get_range_lookup(sample))
 
     with open("inputs/day_5.txt") as f:
         lines = f.readlines()
